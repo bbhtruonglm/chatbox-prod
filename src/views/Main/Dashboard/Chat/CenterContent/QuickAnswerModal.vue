8d6979fca1627e84ed168a6c58e0c6c48b84559c
jackhoangdev <jackhoang@gmail.com>
2023-09-27 11:03:15 +0700

    fix bugs quick answer

--- src/views/Main/Dashboard/Chat/CenterContent/QuickAnswerModal.vue
+++ src/views/Main/Dashboard/Chat/CenterContent/QuickAnswerModal.vue
@@ -5,13 +5,22 @@
            Trả lời nhanh
         </template>
         <template v-slot:body>
-            <div v-for="answer in quick_answers" class=" border-b py-2 px-1.5 cursor-pointer hover:bg-slate-100">
-                <div 
-                    class=" bg-gray-500 text-white w-fit px-1 rounded text-xs font-bold"
-                >
-                    {{ answer.title }}
+            <div class="h-[150px]" v-if="loading">
+                <Loading type="FULL" />
+            </div>
+            <div class="h-[600px] overflow-y-auto" v-if="!loading">
+                <div class="flex justify-between bg-green-100 text-green-500 px-1 py-2 sticky top-0">
+                    <p>Enter hoặc click để chọn</p>
+                    <p>Dùng ↑ hoặc ↓ để chọn</p>
                 </div>
-                <p>{{ answer.content }}</p>
+                <div v-for="answer in quick_answers" class=" border-t py-2 px-1.5 cursor-pointer hover:bg-slate-100">
+                    <div 
+                        class=" bg-gray-500 text-white w-fit px-1 rounded text-xs font-bold"
+                    >
+                       <p class= " truncate max-w-xs" >{{ answer.title }}</p>
+                    </div>
+                    <p>{{ answer.content }}</p>
+                </div>
             </div>
         </template>
     </ModalBottom>
@@ -25,6 +34,7 @@ import { teleportCenterModelOnPcScreen } from '@/service/function'
 import { get_quick_answer } from '@/service/api/chatbox/widget'
 
 import ModalBottom from '@/components/ModalBottom.vue'
+import Loading from '@/components/Loading.vue'
 
 import type { ComponentRef } from '@/service/interface/vue'
 import type { QuickAnswerInfo } from '@/service/interface/app/message'
@@ -37,6 +47,8 @@ const conversationStore = useConversationStore()
 const quick_anser_modal_ref = ref<ComponentRef>()
 /** Danh sách trả lời nhanh */
 const quick_answers = ref<QuickAnswerInfo[]>([])
+/** Trạng thái loading */
+const loading = ref<boolean>(true)
 
 watch(
     () => conversationStore.select_conversation,
@@ -60,6 +72,7 @@ function getQuickAnswer() {
         fb_page_id: page_id
     }, (e, r) => {
         quick_answers.value = r
+        loading.value = false
     })
 }
 
