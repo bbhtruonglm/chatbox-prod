88da0f7d2af0b89627b8f8997a6c822a23fba95a
Mike Nguyen <mike@botbanhang.vn>
2023-12-24 16:16:14 +0700

    update capgo

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,11 +1,17 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'hybrid.chatbox.botbanhang.vn',
+  appId: 'chatbox.botbanhang.vn',
   appName: 'chatbox',
   webDir: 'dist',
   server: {
     androidScheme: 'https'
+  },
+  plugins: {    
+    CapacitorUpdater: {
+      autoUpdate: true,
+      privateKey: '-----BEGIN RSA PRIVATE KEY-----\nMIIEowIBAAKCAQEAl2dlTYAnajd24NHOfJvQP8A//q82HAu0cnxXPNIH9zjK8NlF\ng/Q2kmsWrLAkazf8dCFvL4LKIu4xDm7cM5MLvdYStZdeUI34a5o5NzEkMhI+9qAK\ngdzWNZz5YFqfDs9UdsT3EoVQ7ek+EmD9P9X4S0D0ni2NHBN1oQfyd959B7Qetunk\nmYvVeJ0h8zDG6WLOkNz9y91MuwiL8lYNXF8xVmoABxHse3cJUrXJcUwShFgaD6lv\n4G0stz97+c0AHxxxQspj+w/A9iaICib1BHebNJ9b1BJMBb0XBbhFZ2fBxdfn9m5w\n5ohimEQounHcodJrA6M13IpHHzestXzDk1VqHQIDAQABAoIBAACWJy3CPQFLx7YB\nsvVrERw+HWwE1DEq9Kmri6Ux5VOPTRkTw7xmWiH994v3DPJmuJFRRZ5w8dNNe9BH\ngBxw0NZF/UGtSBwk5DYJK7GStbf3DQTy58W8zA5QAQimMTmNy9gVDpeIjctf7rjZ\ngVb9MRrxhySg12+o9sAHSUwusbGG8M0nuaKAYuO9WlT18M1uzruKx6dLnEpT0NuD\niZpUPYvuI8MzK12oIGji6NcvKUJqWdlE2F85YnwOiOy+wfZOvO/hLq9u172KEa+x\nhNLAMe1UABNUaDS7TSABlrXCIZGKAfTA9GjfWrKWu3XFuJSranZtGTLqwarTszI7\niUhkdwECgYEA0Xzd4mukeeDxvh4JiAScJI0yrailB0E4z8a2xPj5EIznkPodypAY\n7RURsPZubY292aRJe8GpokpA4S0JjorjHvfxSMkTOBx9KVEmN5gYV8q2s+Q4Yoos\nydA2gUNLZVuOCkIOdqtgs+nyJnrUjpURZOuOdzG5AZe13HDDedxve90CgYEAuQUV\naD0eiGQ5mjCF7hNwkQOYwBjCJ/YQMVza/ehRhuct0jH+bfFBz3vVqpdSKV4RTXZw\nVjFiBx+rt6WJCE/190HgfbnM+A1HHKMsXFptl7JbTxi0n9kMsWT2WDzOgNKvNOJm\nMt720geD0KsY+LvBkA5wxHo9Po4VOm8Bs/7a40ECgYEAw1FYz7LtN3Wz7z6yRE+I\nBcxgY+nJdGorPg5Yem5oSjYV8eZBwIt5shLi28voSdzOFg5HNUvrjZUNyOTk9DIq\ncMVcAjD36mkliVBQAaWdjL1Ee5VdvQJ3n/Kx3vOPaIFkfcv5EEvZbeXCEH6VkBsM\njLvTqO6DQCjjNxUsLIKKr0ECgYBEcbD9or6MzibQ92pULDTUMHRte7s3dTuAR01r\n6GlgclztCTmKClnCG5lrRhdK3lWgRlRATnxpDjpD8aTDwnDHS0N0vysoCCFqYqKi\nd4FbQMuucwE+sNiVNzg6ZCQwW8Ac3DPaVckQ4V7N7r2KJkrws6bbyCyn23/W4gHL\nSFhJAQKBgCMV9GuoCXZhPhaqybtfUVT4xuFuu7Wo2XEXMSWtLaVzOOTtNkZSZkGN\nnIdN2Us7lWMSsI90jbkT/jT6vLQhkb++LIG1Ws+hGqmAdyevpnDIEfy8AILJQP+u\nTwu8rgd572mUaXJG7jZYlyU6FfBjQfSQox9G+McLPJya6xya8TIc\n-----END RSA PRIVATE KEY-----\n'
+    }
   }
 };
 
--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,19 @@
     class "container-fluid" để intergrate với ext, không phải là bootstrap
     không được xoá
   -->
-  <div class="w-screen h-screen bg-white select-none md:select-auto container-fluid">
+  <div
+    class="w-screen h-screen bg-white select-none md:select-auto container-fluid"
+  >
+    <div
+      v-if="isNewVersion"
+      class="fixed top-0 left-0 w-full h-8 bg-black bg-opacity-50 z-50 flex justify-center items-center"
+    >
+      <div class="bg-white rounded-lg shadow-lg p-4">
+        <div class="text-center">
+          <div class="text-2xl font-bold">New version available</div>
+        </div>
+      </div>
+    </div>
     <Network />
     <AdBlocker />
     <Loading v-if="commonStore.is_loading_full_screen" type="FULL" />
@@ -14,13 +26,34 @@
 </template>
 
 <script setup lang="ts">
-import { useCommonStore } from '@/stores'
+import { ref } from "vue";
+import { useCommonStore } from "@/stores";
 
-import Loading from '@/components/Loading.vue'
-import Network from './components/Network.vue'
-import AdBlocker from './components/AdBlocker.vue'
+import Loading from "@/components/Loading.vue";
+import Network from "./components/Network.vue";
+import AdBlocker from "./components/AdBlocker.vue";
 
-const commonStore = useCommonStore()
+import { CapacitorUpdater } from "@capgo/capacitor-updater";
+
+const commonStore = useCommonStore();
+
+// Capacitor Update OTA
+const isNewVersion = ref<boolean>(false);
+CapacitorUpdater.notifyAppReady();
+CapacitorUpdater.addListener("majorAvailable", (info: any) => {
+  console.log(">>> Capgo majorAvailable", info);
+  isNewVersion.value = true;
+  setTimeout(() => {
+    isNewVersion.value = false;
+  }, 5000);
+});
+CapacitorUpdater.addListener("updateAvailable", (info: any) => {
+  console.log(">>> Capgo updateAvailable", info);
+  isNewVersion.value = true;
+  setTimeout(() => {
+    isNewVersion.value = false;
+  }, 5000);
+});
 </script>
 
 <style lang="scss">
