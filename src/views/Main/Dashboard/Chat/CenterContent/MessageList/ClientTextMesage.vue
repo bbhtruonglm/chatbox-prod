90b8ad7f05b7aed825ea0f7710d4c999a0d3885e
minhnl <minhnl@botbanhang.vn>
2023-12-25 09:44:36 +0700

    test build: xoa log

--- src/views/Main/Dashboard/Chat/CenterContent/MessageList/ClientTextMesage.vue
+++ src/views/Main/Dashboard/Chat/CenterContent/MessageList/ClientTextMesage.vue
@@ -32,8 +32,6 @@ function renderText(text: string) {
     let email_regex = regex_email.exec(text);
     let url_regex = regex_url.exec(text);
 
-    console.log(email_regex)
-
     // Check phone regex contain url
     if (
         phone_regex &&
