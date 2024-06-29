86029f940ad3e2b9a1603a4baef4df4e7cb9cd68
minhnl <minhnl@botbanhang.vn>
2024-06-29 14:17:02 +0700

    done pay

--- src/views/Dashboard/Org/Pay.vue
+++ src/views/Dashboard/Org/Pay.vue
@@ -2,8 +2,12 @@
   <AccountInfo />
   <PackInfo type="FREE" />
   <PackInfo type="MONTH" />
+  <Analytic />
+  <History />
 </template>
 <script setup lang="ts">
 import AccountInfo from '@/views/Dashboard/Org/Pay/AccountInfo.vue'
 import PackInfo from '@/views/Dashboard/Org/Pay/PackInfo.vue'
+import Analytic from '@/views/Dashboard/Org/Pay/Analytic.vue'
+import History from '@/views/Dashboard/Org/Pay/History.vue'
 </script>
