af17daab72b3169009c16e8a978e0c928ee15acc
minhnl <minhnl@botbanhang.vn>
2024-10-06 22:49:33 +0700

    fix build

--- src/global.d.ts
+++ src/global.d.ts
@@ -1,29 +1,22 @@
 /*******************************************************************************
- *
+ * 
  * * CÀI ĐẶT CÁC BIẾN TOÀN CỤC Ở ĐÂY
- *
+ * 
  ******************************************************************************/
 
-import Vue from 'vue'
 import type { IndexedDB } from '@/service/helper/indexedDB'
 
 declare global {
-  /**các cài đặt của môi trường */
-  var $env: EnvInfo
-  /**NODE_ENV */
-  var $node_env: string
-  /**đối tượng index db */
-  var $indexed_db: IndexedDB
-  /**gắn cờ trình check adblock đã hoạt động */
-  var check_ad_blocker: boolean
-  /**gắn cờ trình phát hiện adblock đã hoạt động */
-  var detect_ad_blocker: boolean
-}
-
-declare module '@vue/runtime-core' {
-  export interface ComponentCustomProperties {
-    $t: (key: string, ...args: any[]) => string
-  }
+    /**các cài đặt của môi trường */
+    var $env: EnvInfo
+    /**NODE_ENV */
+    var $node_env: string
+    /**đối tượng index db */
+    var $indexed_db: IndexedDB
+    /**gắn cờ trình check adblock đã hoạt động */
+    var check_ad_blocker: boolean
+    /**gắn cờ trình phát hiện adblock đã hoạt động */
+    var detect_ad_blocker: boolean
 }
 
 export {}
