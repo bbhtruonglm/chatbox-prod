31b4abab1fd8302cc5a92a9cb8243229273b6b8c
minhnl <minhnl@botbanhang.vn>
2024-10-15 23:29:26 +0700

    use voucher

--- src/utils/api/Billing.ts
+++ src/utils/api/Billing.ts
@@ -34,3 +34,42 @@ export class BillingAppOrganization extends Billing {
     return this.post('update_org', { org_id, ...body })
   }
 }
+
+/**xác minh người dùng thoả mãn điều kiện dùng mã này */
+export interface ResponseVerifyVoucher {
+  /**kiểm tra xem mã giảm giá có hợp lệ không */
+  is_verify?: boolean
+  /**số tiền của giao dịch */
+  txn_amount?: number
+  /**số tiền thực tế mà người dùng nạp vào */
+  txn_origin_amount?: number
+  /**mô tả về mã giảm giá */
+  voucher_description?: string
+}
+
+/**gọi API mã khuyến mại */
+export class BillingAppVoucher extends Billing {
+  constructor() {
+    // gọi API lên module billing
+    super(`app/voucher`)
+
+    // tự động nạp id tổ chức đang chọn
+    this.initSelectedOrgId()
+  }
+
+  /**gọi api post */
+  protected post(path: string, body?: Record<string, any>): Promise<any> {
+    return super.post(path, {
+      org_id: this.org_id,
+      ...body,
+    })
+  }
+
+  /**xác minh người dùng thoả mãn điều kiện dùng mã này */
+  public async verifyVoucher(
+    txn_amount: number,
+    voucher_code: string
+  ): Promise<ResponseVerifyVoucher> {
+    return this.post('verify_voucher', { voucher_code, txn_amount })
+  }
+}
