6407e2030921273e6947e2c79af67a08221af1bc
minhnl <minhnl@botbanhang.vn>
2024-10-17 15:53:08 +0700

    add embed chat

--- src/views/Dashboard/Org.vue
+++ src/views/Dashboard/Org.vue
@@ -13,8 +13,11 @@
         <ReChargeBtn v-if="$route.path.includes('/dashboard/org/pay')" />
       </div>
       <RouterView v-if="orgStore.isAdminOrg()" />
-      <div v-else class="text-sm text-slate-500">
-        {{ $t('v1.view.main.dashboard.org.permision_denied')  }}
+      <div
+        v-else
+        class="text-sm text-slate-500"
+      >
+        {{ $t('v1.view.main.dashboard.org.permision_denied') }}
       </div>
     </template>
   </DashboardLayout>
@@ -28,6 +31,21 @@ import Menu from '@/views/Dashboard/Org/Menu.vue'
 import SelectOrg from '@/components/Main/Dashboard/SelectOrg.vue'
 import ReChargeBtn from '@/views/Dashboard/Org/ReChargeBtn.vue'
 import Loading from '@/components/Loading.vue'
+import { onBeforeMount, onMounted } from 'vue'
 
 const orgStore = useOrgStore()
+
+onMounted(() => {
+  // @ts-ignore
+  BBH?.init({
+    // kích hoạt id trang chat
+    page_id: 'bf425487afbe403895116dd9b585537b',
+    // thiết lập hiển thị - chưa có logic
+    config: {},
+    // cho phép gỡ lỗi
+    is_debug: true,
+  })
+})
+
+// function 
 </script>
